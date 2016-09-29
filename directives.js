810f20a2a0ddc0eef525efbaf0e8b54c5464e097
Developer <Developer>
2016-09-30 03:22:20 +0530

    Angular custom directive to show current system time(updating in every second)

---
+++ directives.js
@@ -0,0 +1,17 @@
+app.directive('currentTime', function ($interval) {
+    return {
+        scope: true, // isolate
+        transclude: true, // bring in any text or elements
+        template: "<span class='clock'><span class='clock-text' ng-transclude></span><span class='clock-time'>{{date.now() | date: timeFormat}}</span></span>",
+        /**
+         * $s scope
+         * $e element
+         * $a attributes
+         */
+        link: function ($s, $e, $a) {
+            $s.timeFormat = ($a.format === '12') ? 'hh:mm:ss a' : 'HH:mm:ss';
+            $s.date = Date;
+            $interval(null, 1000);
+        }
+    };
+});
